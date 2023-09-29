18de41449d6ae5af15fbbbf4a3257aef95265fd1
WilbertRas <w.ras@sron.nl>
2023-09-29 17:39:49 +0200

    add attenuator

--- vna.py
+++ vna.py
@@ -3,29 +3,28 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def get_s21(start, stop, num_points, timeout=3000):
-    freqs = np.linspace(start, stop, num_points)
+def get_s21(f0, bw, num_points, power, ifbw, timeout=3000000):
+    freqs = np.linspace(f0-bw/2, f0+bw/2, num_points)
     VISA = "GPIB0::16::INSTR"
     try:
         rm = pyvisa.ResourceManager() 
         try_visa = 1
     except:
         try_visa = 0
-        print('NOT connected to VNA')
+        print('No pyvisa')
     if try_visa:
         vna = rm.open_resource(VISA)
         vna.timeout = timeout
-        vna.read_termination = '\n'
-        vna.write_termination = '\n'
         if vna.query('*IDN?'):
             print("Connected to VNA:", vna.query('*IDN?'))
             # Configure VNA parameters (example)
             vna.write(f'CALC1:PAR:S21:DEF')  # Define S21 measurement
-            vna.write(f'SENS1:FREQ:START {start}GHz')
-            vna.write(f'SENS1:FREQ:STOP {stop}GHz')
+            vna.write(f'SENS1:FREQ:CENT {f0}GHz')
+            vna.write(f'SENS1:FREQ:SPAN {bw}GHz')
+            vna.write(f'SENS1:BWID {ifbw}Hz')
             vna.write(f'SENS1:SWE:POIN {num_points}')  # Set the number of sweep points
             vna.write(f'SENS1:PAR:S21:FORM MLOG')
-            # vna.write(f'SENS1:POW:ATT AREC,{att}')
+            vna.write(f'SOUR1:POW1:LEV {power}')
 
             # Trigger a single sweep
             vna.write(f'INIT:IMM')
@@ -56,6 +55,25 @@ def get_s21(start, stop, num_points, timeout=3000):
         s21 = np.zeros(num_points)
     return freqs, s21
 
+
+def set_weinschell(attn):
+    VISA = "GPIB0::10::INSTR"
+    attn_chan2 = 2
+    try:
+        rm = pyvisa.ResourceManager() 
+        try_visa = 1
+    except:
+        try_visa = 0
+        print('NO Pyvisa')
+    if try_visa:
+        weinshell = rm.open_resource(VISA)
+        print(weinshell.query(f'*IDN?'))
+        weinshell.write(f'CHAN1;ATTN {attn}')
+        weinshell.write(f'CHAN2;ATTN {attn_chan2}')
+        # print(weinshell.query(f'CHAN 1;ATTN?'))
+        # print(weinshell.query(f'CHAN 2;ATTN?'))
+        weinshell.query(f'*OPC?')
+            
 
 def plot_s21(freqs, s21):
     fig, ax = plt.subplots()
