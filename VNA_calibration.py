9322b2e4028d74181f4164cb018bbb6b091f8e49
wilbertras <ras.wilbert@gmail.com>
2024-11-04 11:52:01 +0100

    fix vna calalibration

--- VNA_calibration.py
+++ VNA_calibration.py
@@ -5,7 +5,19 @@ import pyvisa
 
 if __name__ == "__main__":
     vna = connect2vi("GPIB0::16::INSTR", timeout=3000000)
-    # weinschell = connect2vi("GPIB0::10::INSTR", timeout=300000)
+
     init_vna(vna, calibfile='D:\KIDS\KIDs.csa')
+
+    vna_scan(vna, 5, 2, 3201, -110, 1000, 1)
+
     calibrate_vna(vna)
+
+    if vna.query('*OPC?'):
+        response = vna.query_ascii_values('CALC1:DATA? FDATA;')
+        s21 = np.array(response)
+
+    fig, ax = plt.subplots()
+    ax.plot(s21)
+    
     vna.close()
+    plt.show()
