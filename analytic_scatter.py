e2235dfbec1672ab800e1f50089fe5a3a09da7c0
wilbertr <125577618+wilbertr@users.noreply.github.com>
2025-02-24 17:50:05 +0100

    analyse 1024 chip

--- analytic_scatter.py
+++ analytic_scatter.py
@@ -3,59 +3,61 @@ import matplotlib.pyplot as plt
 from scipy.stats import norm
 
 
-df = 4e6
-f0 = 6e9
-f1 = f0 + df
+f0 = 4e9
+oct = 1
 Q = 50e3
-delta = 20
-N = 1000
-Pr_carlo = []
-Pr_semi = []
-nr_sigma = 10
-yields = []
+delta = 20  # fwhm
 
-def carlo(sigma, plot=False):
-    f = np.linspace(f0-nr_sigma*sigma*f0, f0+nr_sigma*sigma*f0, N)
+# oct1: 1.6e3
+# oct2: 3.2e3
+# oct3: 5.2e3
+# oct4: 6.5e3
+
+def carlo(sigma, df, plot=False):
     a = np.random.normal(f0, sigma*f0, N)
-    b = np.random.normal(f1, sigma*f1, N)
-    Pr = np.sum(b - a > delta) / N
-    if plot:
-        fig, ax = plt.subplots()
-        ax.hist(a, bins='auto')
-        ax.hist(b, bins='auto')
-        ax.set_title(f'Probability is {Pr}')
-        plt.show()
+    b = np.random.normal(f0+df, sigma*(f0+df), N)
+    Pr = np.sum(b - a > delta * a/Q) / N
     return Pr
 
 
-def semi_analytic(sigma):
+def semi_analytic(sigma, df):
     f = np.linspace(f0-nr_sigma*sigma*f0, f0+nr_sigma*sigma*f0, N)
-    Q = norm.sf(f+delta, f1, sigma*f1)
-    p = norm.pdf(f, f0, sigma*f0)
-    Pr = np.sum(p*Q*(2*nr_sigma*sigma*f0)/N)
+    Q_func = norm.sf(f+delta*f0/Q, f0+df, sigma*(f0+df))
+    pdf = norm.pdf(f, f0, sigma*f0)
+    Pr = np.sum(pdf*Q_func*(2*nr_sigma*sigma*f0)/N)
     return Pr
 
-def res_yield(sigma, nr_f0s):
-    f0s = np.linspace(4e9+df, 6e9-df, nr_f0s, endpoint=True)
-    nr_f0s = len(f0s)
-    fs = np.random.normal(f0s, sigma*f0s, (N, nr_f0s))
-    nr = np.sum((fs[:, 1:] - fs[:, :-1]) > (delta*f0s[1:]/Q).T, axis=1)
-    # nr -= np.sum(fs < 4e9, axis=1)
-    # nr -= np.sum(fs > 6e9, axis=1)
-    return np.mean(nr / (nr_f0s-1), axis=0)
-
-
+# N = 10000
+# df = 2e6
+# nr_sigma = 10
 # sigmas = np.logspace(-5, 0)
+# Pr_carlo = []
+# Pr_semi = []
 # for sigma in sigmas:
-#     Pr_semi.append(semi_analytic(sigma))
-#     Pr_carlo.append(carlo(sigma))
+#     Pr_semi.append(semi_analytic(sigma, df))
+#     Pr_carlo.append(carlo(sigma, df))
 # fig, ax = plt.subplots()
 # ax.semilogx(sigmas, Pr_semi, label='semi-analytic') 
 # ax.semilogx(sigmas, Pr_carlo, label='carlo') 
 # ax.legend()
 # plt.show()
 
+def res_yield(sigma, nr_f0s):
+    f_d = rel_freqs(nr_f0s)
+    nr_fs = len(f_d)
+    f_m = np.random.normal(f_d, sigma*f_d, (N, nr_f0s))
+    nr = np.sum((f_m[:, 1:] - f_m[:, :-1]) > (delta*f_d[1:]/Q).T, axis=1)
+    return np.mean(nr / (nr_fs-1), axis=0), np.std(nr / (nr_fs-1), axis=0)
 
+def rel_freqs(nr):
+    f_end = 2**oct * f0
+    x = Q*(2**(oct/nr)-1)
+    powers = np.arange(nr)
+    freqs = f0 * (1+x/Q)**powers
+    return freqs
+
+
+N = 100
 sigmas = np.logspace(-5, 0)
 nrs = np.logspace(1, 4, 4, endpoint=True)
 fig, ax = plt.subplots()
@@ -63,20 +65,32 @@ for nr in nrs:
     yields = []
     for sigma in sigmas:
         yields.append(res_yield(sigma, int(nr)))
-    ax.semilogx(sigmas, yields, label=f'nr_f0s={nr}')
+    yields = np.stack(yields)    
+    ax.semilogx(sigmas, yields[:,0], label=f'nr_f0s={nr}')
+    ax.fill_between(sigmas, yields[:,0]-yields[:,1], yields[:,0]+yields[:,1], alpha=.5)
 ax.legend()
+ax.set_xlabel(['# kids'])
+ax.set_ylabel(['expected yield'])
 ax.set_title('yield vs sigma for various nr_f0s')
 plt.show()
 
-
-# nrs = np.logspace(1, 4, 30, endpoint=True)
+# N = 10
+# nrs = np.logspace(1, 5, 40, endpoint=True)
+# sigmas = np.logspace(-5, -2, 4, endpoint=True)
 # yields2 = []
 # fig, ax = plt.subplots()
-# for sigma in np.logspace(-5, -2, 4, endpoint=True):
+# for sigma in sigmas:
 #     yields2 = []
 #     for nr in nrs:
-#         yields.append(res_yield2(sigma, int(nr)))
-#     ax.semilogx(nrs, yields2, label=f'sigma={sigma}')
+#         yields2.append(res_yield(sigma, int(nr)))
+#     yields2 = np.stack(yields2)    
+#     ax.semilogx(nrs, yields2[:,0], label=f'$\sigma/f$=1E$^%d$' % (np.log10(sigma)))
+#     ax.fill_between(nrs, yields2[:,0]-yields2[:,1], yields2[:,0]+yields2[:,1], alpha=.5)
+# ax.axvline(oct/np.log2(delta/Q+1), c='k', ls='--', label='$oct/(\mathrm{log}_2(\lambda/Q+1))$')
+# ax.set_xlabel('# kids')
+# ax.set_ylabel('expected yield')
+# ax.set_ylim(0, 1.01)
+# ax.set_xlim(nrs[0], nrs[-1])
 # ax.legend()
 # ax.set_title('yield vs nr kids for various sigma')
 # plt.show()
