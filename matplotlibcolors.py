ce91f2f01e645260c18e825538f88a501d94d9fb
wilbertr <125577618+wilbertr@users.noreply.github.com>
2025-11-20 10:56:38 +0100

    big commit before mapping

--- matplotlibcolors.py
+++ matplotlibcolors.py
@@ -4,7 +4,7 @@ def redefine_colors():
     # Redefine color shortcuts
     mcolors.ColorConverter.colors['b'] = "#0072BD"  # Change 'b' (blue) to a new color
     mcolors.ColorConverter.colors['o'] = "#D95319"  # Change 'r' (red) to a new color
-    mcolors.ColorConverter.colors['g'] = "#77AC30"  # Change 'g' (green) to a new color
+    mcolors.ColorConverter.colors['g'] = "#3BAA32"  # Change 'g' (green) to a new color
     mcolors.ColorConverter.colors['y'] = "#EDB120"  # Change 'y' (yellow) to a new color
     mcolors.ColorConverter.colors['p'] = "#7E2F8E"  # Change 'm' (magenta) to a new color
     mcolors.ColorConverter.colors['lb'] = "#4DBEEE"  # Change 'c' (cyan) to a new color
