1320d1d9409c43bb424f00be6372bc060cecd12b
wilbertr <125577618+wilbertr@users.noreply.github.com>
2025-11-26 10:16:53 +0100

    mapped LT402chip1

--- powerdown_phone.py
+++ powerdown_phone.py
@@ -2,7 +2,7 @@ from ppadb.client import Client as AdbClient
 import os
 
 try:
-    os.startfile("scrcpy-win64-v211\scrcpy.exe")
+    # os.startfile("scrcpy-win64-v211\scrcpy.exe")
     client = AdbClient(host="127.0.0.1", port=5037) # Default is "127.0.0.1" and 5037
 except:
     print('No phone connected')
@@ -10,5 +10,7 @@ devices = client.devices()
 if len(devices) == 0:
     print('No devices')
     quit()
+else:
+    print('devices: ', devices)
 device = devices[0]
 device.shell("reboot -p")
