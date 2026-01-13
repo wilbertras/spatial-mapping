13aeac334a2178eb117f91b4e871bc3e3aaedf76
wilbertras <ras.wilbert@gmail.com>
2026-01-13 20:52:31 +0100

    map LT402chip4trim

--- VNAsweep.py
+++ VNAsweep.py
@@ -1,31 +1,10 @@
 from functions import *
 import time
 
-# if __name__ == "__main__":
-#     fstart = 5        # GHz
-#     fstop = 6       # GHz
-#     scanbw = 1000       # MHz
-#     nr_points = 6401
-#     power = -110        # dBm
-#     ifbw = 10000         # Hz
-
-#     st = time.time()
-#     freqs, s21 = get_s21(fstart, fstop, scanbw, nr_points, power, ifbw, calfile=False)
-#     et = time.time()
-#     elapsed_time = et - st
-#     print('Elapsed time = %d seconds' % elapsed_time)
-
-#     fig = plot_s21(freqs, s21)
-#     plt.show()
-#     file_path = save_numpy_array(np.stack((freqs, s21), axis=-1).T)
-#     if file_path:
-#         fig.savefig(file_path.split('.')[0] + '.png')
-
-
 if __name__ == "__main__":
-    fstart = 5        # GHz
-    fstop = 6       # GHz
-    scanbw = 1000       # MHz
+    fstart = 4       # GHz
+    fstop = 4.5      # GHz
+    scanbw = 500       # MHz
     nr_points = 6401
     power = -110        # dBm
     ifbw = 10000         # Hz
@@ -35,3 +14,24 @@ if __name__ == "__main__":
     et = time.time()
     elapsed_time = et - st
     print('Elapsed time = %d seconds' % elapsed_time)
+
+    fig = plot_s21(freqs, s21)
+    plt.show()
+    file_path = save_numpy_array(np.stack((freqs, s21), axis=-1).T)
+    if file_path:
+        fig.savefig(file_path.split('.')[0] + '.png')
+
+
+# if __name__ == "__main__":
+#     fstart = 8       # GHz
+#     fstop = 8.5      # GHz
+#     scanbw = 500       # MHz
+#     nr_points = 6401
+#     power = -110        # dBm
+#     ifbw = 10000         # Hz
+
+#     st = time.time()
+#     freqs, s21 = get_s21(fstart, fstop, scanbw, nr_points, power, ifbw, calfile=False)
+#     et = time.time()
+#     elapsed_time = et - st
+#     print('Elapsed time = %d seconds' % elapsed_time)
